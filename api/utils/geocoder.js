7284d98f66159a7d623ae87aa817f3deea8940e6
roman <romaxa83@ukr.net>
2020-01-26 16:02:32 +0200

    geocoder

---
+++ api/utils/geocoder.js
@@ -0,0 +1,14 @@
+// подключение к geocoder
+// @see https://developer.mapquest.com
+const NodeGeocoder = require('node-geocoder');
+
+const options = {
+    provider: process.env.GEOCODER_PROVIDER,
+    httpAdapter: 'https',
+    apiKey: process.env.GEOCODER_API_KEY,
+    formatter: null
+};
+
+const geocoder = NodeGeocoder(options);
+
+module.exports = geocoder;
